1291fed2631329aa8f4716b446db54d7501d3b33
Trung <du.do@mum.edu>
2019-05-17 12:08:35 -0500

    Add more files for block attendance (task 5)

---
+++ src/main/java/edu/mum/tmcheck/domain/repository/BlockAttendanceReportRepository.java
@@ -0,0 +1,16 @@
+package edu.mum.tmcheck.domain.repository;
+
+import edu.mum.tmcheck.domain.reports.BlockAttendanceReport;
+import edu.mum.tmcheck.domain.reports.EntryAttendanceReport;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BlockAttendanceReportRepository extends CrudRepository<BlockAttendanceReport, String> {
+    @Query(value = "SELECT b FROM BlockAttendanceReport AS b WHERE b.block_start = :block_start and b.block_end = :block_end")
+    public List<BlockAttendanceReport> findByBlock(LocalDate block_start, LocalDate block_end);
+}
